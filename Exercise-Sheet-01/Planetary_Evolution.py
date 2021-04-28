d22eebd1fbb6a4b4932bad1a9375338ba4e5d6ca
matteogarbellini <matteo.garbellini@studenti.unimi.it>
2021-04-28 20:31:12 +0200

    Add Object-Planet and System Class

--- Exercise-Sheet-01/Planetary_Evolution.py
+++ Exercise-Sheet-01/Planetary_Evolution.py
@@ -6,3 +6,38 @@ Albert-Ludwigs-Universität Freiburg
 Computational Physics: Material Science
 Exercise Sheet 01
 """
+
+
+import numpy as np
+
+
+class Object:
+    def __init__(self, name, mass, init_position, init_velocity):
+        self.name = name
+        self.mass = mass
+        self.init_position = init_position
+        self.init_velocity = init_velocity
+        self.pos = [self.init_position,]
+        self.vel = [self.init_velocity,]
+
+class System:
+    def __init__(self, sun):
+        self.sun = sun
+        self.planets = []
+        self.timestamp = 0
+
+    def add_planet(self, planet):
+        self.planet.append(planet)
+
+    def evolve(self, timestep, integrator)
+        for planet in self.planets :
+            if (integrator == "E"):
+                run_euler_integrator()
+            elif (integrator == "V"):
+                run_verlet_integrator()
+            elif (integrator == "VV")
+                run_velocity_verlet_integrator()
+            else:
+                print("Error: undefined integrator type.
+                        \n Please enter E - Euler,
+                        V - Verlet, VV - Velocity Verlet")
